798084042e7e820ed31c1d433b18aa854830e18e
ishu3101 <ishu3101@hotmail.com>
2016-11-12 21:24:51 +1300

    Update deploy script.

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -3,9 +3,11 @@
 git config --global user.name ${GITHUB_NAME}
 git config --global user.email ${GITHUB_EMAIL}
 
+# create a new branch without any commit. It keeps the index and the working tree files intact
+git checkout --orphan gh-pages
+
 resume export index.html
-
 git add index.html
-git commmit -m "Deploy at `date +"%Y-%m-%d %H:%M"`"
+git commit -m "Deploy at `date +"%Y-%m-%d %H:%M"`"
 
-git push --force --quiet "https://${GITHUB_TOKEN}@{GITHUB_REPO}.git" master:gh-pages
+git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REPO}.git" gh-pages
